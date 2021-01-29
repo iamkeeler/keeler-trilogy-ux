9036913f6016a7352269fabc3103170d415e10bc
Gary <iamkeeler@gmail.com>
2021-01-28 21:41:01 -0600

    changes during class 19.1

--- Demos/19.1/js/index.js
+++ Demos/19.1/js/index.js
@@ -1,18 +1,40 @@
+function doSomething() {
+    var x = 7;
+    var y = 3;
+    var five = x + y;
+    console.log(five); 
+}
+
+doSomething();
+
 //Example 1 of variables being used for addition
-var x = 20;
-var y = 10;
-var sum = x + y;
-console.log(sum);
+// var x = 20;
+// var y = 10;
+// var sum = x + y;
+// console.log(sum);
 
 //Example 2 of variables being used to contain a string
-var a = "Big Kahuna Burger";
-console.log(a);
+// var a = "Really Big Kahuna Burger";
+// console.log(a);
 
 //Example 3 of concatenation using variables to form a sentence
-var adj = "fat";
-var noun = "dog";
-var madlib = "The " + adj + " " + noun + " " + "jumps in the river";
-console.log(madlib);
+// var adj = "fat";
+// var noun = "dog";
+// var madlib = "The " + adj + " " + noun + " " + "jumps in the river";
+// console.log(madlib);
+
+
+// A function to display and alert
+// function alertThing() {
+//     alert(madlib);
+// }
+
+// Actually calling the function 
+// alertThing();
+
+$("#button1").on("click", function() {
+    $('.hideMe').slideToggle();
+})
 
 // event listener
 $(".button").on("click", function() {
@@ -30,4 +52,4 @@ $("#resetButton").on("click", function(){
     $("#thisButton").addClass("black");
 });
 
-$(".button").on("click")
+// $(".button").on("click")
